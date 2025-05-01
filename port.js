c6e128652865df8e885f5bfa2043657b0698b849
Davronbek Ibodov <davronbekibodov9557@gmail.com>
2025-05-01 00:15:32 -0700

    Updated work section and improved labels

--- port.js
+++ port.js
@@ -43,159 +43,183 @@ function updateInnerHTML(selector, html) {
 
 // Tilni o'zgartirish funksiyasi
 function changeLanguage(lang) {
-  const translations = {
-      Uzb: {
-          home: "Bosh sahifa",
-          about: "Haqida",
-          certificates: "Sertifikatlar",
-          contact: "Bog'lanish",
-          introduction: "Men kiberxavfsizlik va veb-dasturlash bo'yicha mutaxassisman.",
-          hireMe: "Meni yollang",
-          downloadCV: "CV-ni yuklab olish",
-          getInTouch: "Bog'laning",
-          email: "Elektron pochta",
-          phone: "Telefon",
-          footerText: "Barcha huquqlar himoyalangan.",
-          scrollDown: "Pastga aylantiring",
-          aboutHeader: "Men haqimda",
-          aboutDescription: "Men — kiberxavfsizlik va axborot xavfsizligi bo‘yicha mutaxassisman. Xavfsizlik siyosatlari, tarmoq va tizim xavfsizligi, zaifliklarni aniqlash va ularni bartaraf etish bo‘yicha tajribaga egaman. Kiber tahdidlarga qarshi ilg‘or himoya choralarini ishlab chiqish hamda xavfsiz infratuzilma yaratish ustida ishlayman. Qo‘shimcha ravishda, web dasturlarda xavfsizlikni ta'minlash maqsadida web development sohasida ham faoliyat yuritaman.",
-          certificatesHeader: "Sertifikatlar",
-          contactHeader: "Bog'lanish",
-          contactSubtext: "Agar loyihangiz bo'lsa, bu yerda bog'laning",
-          findMe: "Meni toping",
-          myIntroduction: "Mening tanishtiruvim",
-          name: "Davronbek",
-          cvFile: "uzb.pdf",
-          send: "Yuborish",
-          pronounText: "Men",
-          typedStrings: ["Kiberxavfsizlik Mutaxassisi", "Veb-Dasturchi", "Axloqiy Xaker"]
-      },
-      Eng: {
-          home: "Home",
-          about: "About",
-          certificates: "Certificates",
-          contact: "Contact",
-          introduction: "I am a cybersecurity and web development specialist.",
-          hireMe: "Hire Me",
-          downloadCV: "Download CV",
-          getInTouch: "Get in touch",
-          email: "Email",
-          phone: "Phone",
-          footerText: "All rights reserved.",
-          scrollDown: "Scroll Down",
-          aboutHeader: "About Me",
-          aboutDescription: "I am a specialist in cybersecurity and information security. I have experience in security policies, network and system security, identifying vulnerabilities, and mitigating them. I work on developing advanced protection measures against cyber threats and creating secure infrastructures. Additionally, I am active in web development to ensure security in web applications.",
-          certificatesHeader: "Certificates",
-          contactHeader: "Get in touch",
-          contactSubtext: "Do you have a project in mind? Contact me here",
-          findMe: "Find Me",
-          myIntroduction: "My Introduction",
-          name: "Davronbek Ibodov",
-          cvFile: "eng.pdf",
-          send: "Send",
-          pronounText: "I'm",
-          typedStrings: ["Cybersecurity Specialist", "Web Developer", "Ethical Hacker"]
-      },
-      Rus: {
-          home: "Главная",
-          about: "О нас",
-          certificates: "Сертификаты",
-          contact: "Контакты",
-          introduction: "Я специалист по кибербезопасности и веб-разработке.",
-          hireMe: "Нанять меня",
-          downloadCV: "Скачать резюме",
-          getInTouch: "Связаться",
-          email: "Электронная почта",
-          phone: "Телефон",
-          footerText: "Все права защищены.",
-          scrollDown: "Прокрутите вниз",
-          aboutHeader: "Обо мне",
-          aboutDescription: "Я специалист по кибербезопасности и информационной безопасности. У меня есть опыт в разработке политик безопасности, защите сетей и систем, выявлении уязвимостей и их устранении. Я занимаюсь разработкой передовых мер защиты от киберугроз и созданием безопасной инфраструктуры. Кроме того, я активно работаю в области веб-разработки для обеспечения безопасности веб-приложений.",
-          certificatesHeader: "Сертификаты",
-          contactHeader: "Связаться",
-          contactSubtext: "У вас есть проект? Свяжитесь со мной здесь",
-          findMe: "Найти меня",
-          myIntroduction: "Мое введение",
-          name: "Давронбек Ибодов",
-          cvFile: "rus.pdf",
-          send: "Отправить",
-          pronounText: "Я",
-          typedStrings: ["Специалист по кибербезопасности", "Веб-разработчик", "Этический хакер"]
-      }
-  };
+    const translations = {
+        Eng: {
+            home: '🏠',
+            about: 'About',
+            work: 'Work Exp',
+            certificates: 'Certificates',
+            contact: 'Contact',
+            'work-header': 'Work Experience',
+            'work-placeholder': 'Your work experience will be displayed here.',
+            introduction: "I am a cybersecurity and web development specialist.",
+            hireMe: "Hire Me",
+            downloadCV: "Download CV",
+            getInTouch: "Get in touch",
+            email: "Email",
+            phone: "Phone",
+            footerText: "All rights reserved.",
+            scrollDown: "Scroll Down",
+            aboutHeader: "About Me",
+            aboutDescription: "I am a specialist in cybersecurity and information security. I have experience in security policies, network and system security, identifying vulnerabilities, and mitigating them. I work on developing advanced protection measures against cyber threats and creating secure infrastructures. Additionally, I am active in web development to ensure security in web applications.",
+            certificatesHeader: "Certificates",
+            contactHeader: "Get in touch",
+            contactSubtext: "Do you have a project in mind? Contact me here",
+            findMe: "Find Me",
+            myIntroduction: "My Introduction",
+            name: "Davronbek",
+            cvFile: "eng.pdf",
+            send: "Send",
+            pronounText: "I'm",
+            typedStrings: ["Cybersecurity Specialist", "Web Developer", "Ethical Hacker"]
+        },
+        Uzb: {
+            home: '🏠',
+            about: 'Haqida',
+            work: 'Ish',
+            certificates: 'Sertifikatlar',
+            contact: 'Aloqa',
+            'work-header': 'Ish tajribasi',
+            'work-placeholder': 'Bu yerda ish tajribangiz ko‘rsatiladi.',
+            introduction: "Men kiberxavfsizlik va veb-dasturlash bo'yicha mutaxassisman.",
+            hireMe: "Meni yollang",
+            downloadCV: "CV-ni yuklab olish",
+            getInTouch: "Bog'laning",
+            email: "Elektron pochta",
+            phone: "Telefon",
+            footerText: "Barcha huquqlar himoyalangan.",
+            scrollDown: "Pastga aylantiring",
+            aboutHeader: "Men haqimda",
+            aboutDescription: "Men — kiberxavfsizlik va axborot xavfsizligi bo‘yicha mutaxassisman. Xavfsizlik siyosatlari, tarmoq va tizim xavfsizligi, zaifliklarni aniqlash va ularni bartaraf etish bo‘yicha tajribaga egaman. Kiber tahdidlarga qarshi ilg‘or himoya choralarini ishlab chiqish hamda xavfsiz infratuzilma yaratish ustida ishlayman. Qo‘shimcha ravishda, web dasturlarda xavfsizlikni ta'minlash maqsadida web development sohasida ham faoliyat yuritaman.",
+            certificatesHeader: "Sertifikatlar",
+            contactHeader: "Bog'lanish",
+            contactSubtext: "Agar loyihangiz bo'lsa, bu yerda bog'laning",
+            findMe: "Meni toping",
+            myIntroduction: "Mening tanishtiruvim",
+            name: "Davronbek",
+            cvFile: "uzb.pdf",
+            send: "Yuborish",
+            pronounText: "Men",
+            typedStrings: ["Kiberxavfsizlik Mutaxassisi", "Veb-Dasturchi", "Axloqiy Xaker"]
+        },
+        Rus: {
+            home: '🏠',
+            about: 'Обо мне',
+            work: 'Опыт',
+            certificates: 'Сертификаты',
+            contact: 'Контакт',
+            'work-header': 'Опыт работы',
+            'work-placeholder': 'Ваш опыт работы будет отображен здесь.',
+            introduction: "Я специалист по кибербезопасности и веб-разработке.",
+            hireMe: "Нанять меня",
+            downloadCV: "Скачать резюме",
+            getInTouch: "Связаться",
+            email: "Электронная почта",
+            phone: "Телефон",
+            footerText: "Все права защищены.",
+            scrollDown: "Прокрутите вниз",
+            aboutHeader: "Обо мне",
+            aboutDescription: "Я специалист по кибербезопасности и информационной безопасности. У меня есть опыт в разработке политик безопасности, защите сетей и систем, выявлении уязвимостей и их устранении. Я занимаюсь разработкой передовых мер защиты от киберугроз и созданием безопасной инфраструктуры. Кроме того, я активно работаю в области веб-разработки для обеспечения безопасности веб-приложений.",
+            certificatesHeader: "Сертификаты",
+            contactHeader: "Связаться",
+            contactSubtext: "У вас есть проект? Свяжитесь со мной здесь",
+            findMe: "Найти меня",
+            myIntroduction: "Мое введение",
+            name: "Давронбек",
+            cvFile: "rus.pdf",
+            send: "Отправить",
+            pronounText: "Я",
+            typedStrings: ["Специалист по кибербезопасности", "Веб-разработчик", "Этический хакер"]
+        }
+    };
 
-  const t = translations[lang];
+    document.querySelectorAll('[data-lang]').forEach(el => {
+        const key = el.getAttribute('data-lang');
+        if (translations[lang][key]) {
+            el.textContent = translations[lang][key];
+        }
+    });
 
-  // Update navigation links
-  document.querySelectorAll(".nav-link").forEach((link, index) => {
-      const keys = ["home", "about", "certificates", "contact"];
-      link.textContent = t[keys[index]];
-  });
+    // Adjust navbar layout dynamically
+    const navMenu = document.querySelector('.nav-menu');
+    if (lang === 'Rus' || lang === 'Uzb') {
+        navMenu.style.flexWrap = 'wrap'; // Allow wrapping for longer text
+    } else {
+        navMenu.style.flexWrap = 'nowrap'; // Default layout for English
+    }
 
-  // Update featured section
-  updateTextContent(".featured-text-info", t.introduction);
-  updateTextContent(".btn.blue-btn", t.hireMe);
+    const t = translations[lang];
 
-  // Update "Download CV" buttons
-  document.querySelectorAll(".download-cv-btn").forEach(button => {
-      button.innerHTML = `${t.downloadCV} <i class="uil uil-file-alt"></i>`;
-      button.onclick = () => {
-          const link = document.createElement("a");
-          link.href = t.cvFile;
-          link.download = t.cvFile;
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-      };
-  });
+    // Update navigation links
+    document.querySelectorAll(".nav-link").forEach((link, index) => {
+        const keys = ["home", "about", "work", "certificates", "contact"];
+        link.textContent = t[keys[index]];
+    });
 
-  // Update about section
-  updateTextContent("#about .top-header h1", t.aboutHeader);
-  updateTextContent("#about .about-info h3", t.myIntroduction);
-  updateTextContent("#about .about-info p", t.aboutDescription);
+    // Update featured section
+    updateTextContent(".featured-text-info", t.introduction);
+    updateTextContent(".btn.blue-btn", t.hireMe);
 
-  // Update certificates section
-  updateTextContent("#projects .top-header h1", t.certificatesHeader);
+    // Update "Download CV" buttons
+    document.querySelectorAll(".download-cv-btn").forEach(button => {
+        button.innerHTML = `${t.downloadCV} <i class="uil uil-file-alt"></i>`;
+        button.onclick = () => {
+            const link = document.createElement("a");
+            link.href = t.cvFile;
+            link.download = t.cvFile;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        };
+    });
 
-  // Update contact section
-  updateTextContent("#contact .top-header h1", t.contactHeader);
-  updateTextContent("#contact .top-header span", t.contactSubtext);
-  updateInnerHTML(".contact-info p:nth-child(2)", `<i class="uil uil-envelope"></i> ${t.email}: davronbekibodov9557@gmail.com`);
-  updateInnerHTML(".contact-info p:nth-child(3)", `<i class="uil uil-phone"></i> ${t.phone}: +998 33 734 9557`);
-  updateInnerHTML("#contact .contact-info h2", `${t.findMe} <i class="uil uil-corner-right-down"></i>`);
+    // Update about section
+    updateTextContent("#about .top-header h1", t.aboutHeader);
+    updateTextContent("#about .about-info h3", t.myIntroduction);
+    updateTextContent("#about .about-info p", t.aboutDescription);
 
-  // Update footer
-  updateTextContent(".nav-name", t.name);
-  updateTextContent(".featured-text-card span", t.name);
-  updateTextContent("footer .top-footer p", t.name);
-  updateInnerHTML("footer .bottom-footer p", `Copyright © <a href="#home" style="text-decoration: none;">${t.name}</a> - ${t.footerText}`);
+    // Update certificates section
+    updateTextContent("#projects .top-header h1", t.certificatesHeader);
 
-  // Update scroll down text
-  updateTextContent(".scroll-icon-box p", t.scrollDown);
+    // Update contact section
+    updateTextContent("#contact .top-header h1", t.contactHeader);
+    updateTextContent("#contact .top-header span", t.contactSubtext);
+    updateInnerHTML(".contact-info p:nth-child(2)", `<i class="uil uil-envelope"></i> ${t.email}: davronbekibodov9557@gmail.com`);
+    updateInnerHTML(".contact-info p:nth-child(3)", `<i class="uil uil-phone"></i> ${t.phone}: +998 33 734 9557`);
+    updateInnerHTML("#contact .contact-info h2", `${t.findMe} <i class="uil uil-corner-right-down"></i>`);
 
-  // Update send button
-  updateTextContent(".send-btn", t.send);
+    // Update footer
+    updateTextContent(".nav-name", t.name);
+    updateTextContent(".featured-text-card span", t.name);
+    updateTextContent("footer .top-footer p", t.name);
+    updateInnerHTML("footer .bottom-footer p", `Copyright © <a href="#home" style="text-decoration: none;">${t.name}</a> - ${t.footerText}`);
 
-  // Update pronoun text
-  const pronounTextElement = document.querySelector(".pronoun-text");
-  pronounTextElement.textContent = t.pronounText;
-  pronounTextElement.style.color = "black";
+    // Update scroll down text
+    updateTextContent(".scroll-icon-box p", t.scrollDown);
 
-  // Update Typed.js
-  if (typedInstance) typedInstance.destroy();
-  typedInstance = new Typed(".typedText", {
-      strings: t.typedStrings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 80,
-      backDelay: 2000
-  });
+    // Update send button
+    updateTextContent(".send-btn", t.send);
 
-  // Close dropdown menu
-  document.querySelector("#langList").style.display = "none";
+    // Update pronoun text
+    const pronounTextElement = document.querySelector(".pronoun-text");
+    pronounTextElement.textContent = t.pronounText;
+    pronounTextElement.style.color = "black";
 
-  // Update the language button text when a language is selected
-  document.getElementById("langBtn").innerText = lang;
+    // Update Typed.js
+    if (typedInstance) typedInstance.destroy();
+    typedInstance = new Typed(".typedText", {
+        strings: t.typedStrings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 80,
+        backDelay: 2000
+    });
+
+    // Close dropdown menu
+    document.querySelector("#langList").style.display = "none";
+
+    // Update the language button text when a language is selected
+    document.getElementById("langBtn").innerText = lang;
 }
 
 // Dropdown menyuni ochish va yopish
